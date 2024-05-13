b81eaf5c6731a5ec54f2d79ed0876ee197b0a8c1
leo <leomasellis.cohort238@gmail.com>
2024-05-13 14:57:13 -0700

    fixed card responsiveness and started work on contact page

--- gamecube-website/src/item-card.jsx
+++ gamecube-website/src/item-card.jsx
@@ -5,12 +5,10 @@ export default function Card(props) {
     return(
             <div className="item-card">
                     <img src={props.img} className="item-img" />
-            
                 <div className="item-stats">
                     <p className="product-name">{props.title}</p>
                     <p className="product-price">${props.price}</p>
                 </div>
-                    <button className="cart-button">Add to Cart</button>
             </div>
     )
 }
--- gamecube-website/src/pages/contact.jsx
+++ gamecube-website/src/pages/contact.jsx
@@ -1,6 +1,21 @@
+import "./contact.css"
 
 export default function Contact() {
     return(
-        <h1>Contact Us</h1>
+    <main className="Contact-page">
+        <div className="contact-box-container">
+            <div className="contact-box">
+                <p className="contact-name">Leo Masellis</p>
+                <p>Number: (209) 652-2218</p>
+                <p>Email: leomasellis@gmail.com</p>
+            </div>
+
+            <div className="contact-box">
+                <p className="contact-name">Chad Peters</p>
+                <p>Number: idek</p>
+                <p>Email: idrk</p>
+            </div>
+        </div>
+    </main>        
     )
 }
